4e06c39129333d0f35eb152f92eb075a440868d3
Brian Rodriguez <thatbrod@gmail.com>
2014-12-31 02:48:52 -0500

    Update contiguous_mdim_array.c

--- contiguous_mdim_array.c
+++ contiguous_mdim_array.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Returns a pointer to a contiguous m-dimensional array w/ the given
-dimensions. Does not protect against overflow, so be wary of large values of m
-and dimension sizes. Consider using floating m-dimensional arrays for such cases
-instead. Because it is completely contiguous, you free the entire array by
-freeing the returned base pointer. Implementation further below. */
+/*
+Returns a pointer to a contiguous m-dimensional array w/ the given dimensions.
+Does not protect against overflow, so be wary of large values of m and dimension
+sizes. Consider using floating m-dimensional arrays for such cases instead.
+Because it is completely contiguous, you free the entire array by freeing the
+returned base pointer. Implementation further below.
+
+WARNING: For an [M][N]...[Z] array 'arr', the elements do NOT live in the range:
+	[ 'arr' , 'arr + M*N*...*Z' )
+	^-inclusive                 ^-exclusive
+
+but, instead, in the range:
+	[ '&arr[0][0]...[0]' , '&arr[M-1][N-1]...[Z-1]' ]
+	^-inclusive                                     ^-inclusive
+*/
 void * malloc_mdim_arr(size_t * dim_sizes, size_t m, size_t elm_size, size_t elm_align);
 
 
@@ -13,14 +23,15 @@ void * malloc_mdim_arr(size_t * dim_sizes, size_t m, size_t elm_size, size_t elm
 int
 main(int argc, char ** argv)
 {
+	int i, j, k;
 	size_t dims[3] = { 3, 8, 4 };
 	double *** arr = malloc_mdim_arr(dims, 3, sizeof(double), __alignof__(double));
 
-	for (int i = 0; i < 3; ++i) {
-		for (int j = 0; j < 8; ++j) {
-			for (int k = 0; k < 4; ++k) {
+	for (i = 0; i < 3; ++i) {
+		for (j = 0; j < 8; ++j) {
+			for (k = 0; k < 4; ++k) {
 				arr[i][j][k] = i*j*0.5 - j*k*7.75 + k*i*3.25;
-				printf("arr[%d][%d][%d] = %f\n", i, j, k, arr[i][j][k]);
+				printf("arr[%d][%d][%d]/%x = %f\n", i, j, k, &arr[i][j][k], arr[i][j][k]);
 			}
 		}
 	}
@@ -32,6 +43,9 @@ main(int argc, char ** argv)
 void *
 malloc_mdim_arr(size_t * dim_sizes, size_t m, size_t elm_size, size_t elm_align)
 {
+	size_t ptr_space, pad_space, elm_space, * prod_arr, increments, i;
+	void ** arr, ** ptr_at, * ptr_to;
+
 	if (dim_sizes == NULL || m == 0 || elm_size == 0 || elm_align == 0) {
 		return NULL;
 	}
@@ -40,16 +54,13 @@ malloc_mdim_arr(size_t * dim_sizes, size_t m, size_t elm_size, size_t elm_align)
 		return malloc(dim_sizes[0] * elm_size);
 	}
 
-	size_t ptr_space, pad_space, elm_space, * prod_arr;
-	void ** arr;
-
 	if ((prod_arr = malloc((m - 1) * sizeof(size_t))) == NULL) {
 		return NULL;
 	}
 
 	ptr_space = prod_arr[0] = dim_sizes[0];
 
-	for (size_t i = 1; i < (m - 1); ++i) {
+	for (i = 1; i < (m - 1); ++i) {
 		prod_arr[i] = prod_arr[i - 1] * dim_sizes[i];
 		ptr_space += prod_arr[i];
 	}
@@ -64,22 +75,19 @@ malloc_mdim_arr(size_t * dim_sizes, size_t m, size_t elm_size, size_t elm_align)
 		return NULL;
 	}
 
-	void ** ptr_at, * ptr_to;
 	ptr_at = arr;
 	ptr_to = (void *) arr + dim_sizes[0] * sizeof(void *);
 
-	for (size_t cur_dim = 1; cur_dim < m; ++cur_dim) {
-		size_t increments;
-
-		if (cur_dim == (m - 1)) {
+	for (i = 1; i < m; ++i) {
+		if (i == (m - 1)) {
 			ptr_to += pad_space;
-			increments = dim_sizes[cur_dim] * elm_size;
+			increments = dim_sizes[i] * elm_size;
 
 		} else {
-			increments = dim_sizes[cur_dim] * sizeof(void *);
+			increments = dim_sizes[i] * sizeof(void *);
 		}
 
-		while (prod_arr[cur_dim - 1]--) {
+		while (prod_arr[i - 1]--) {
 			*ptr_at++ = ptr_to;
 			ptr_to += increments;
 		}
